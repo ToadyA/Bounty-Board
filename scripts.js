31e2893b7052ad9fa92e8c3b50e484fc970e8715
Asher Roberts <asher.roberts98@gmail.com>
2024-11-11 16:03:34 -0800

    I added scrolling for the money you rake in clearing a bounty, and added a sound when it finishes counting up. I hid the default poster with its different properties as the parent object, and added some pseudo-physics, so now when a tack overlaps with another tack, it gets repelled somewhat into a random direction. There are some issues like the bounty scrolling left at larger numbers, and the tack repel force sometimes not resulting in the tacks' displacement, but otherwise I am pretty happy with the final state of things. Project complete!

--- scripts.js
+++ scripts.js
@@ -39,17 +39,28 @@ document.getElementById("posterStack").onclick = function(){
 
 function createNewPoster(outlaw, infamy, bountySum, stars){
 	if(isNaN(bountySum)){
-		alert("Is that a number, pardner?");
+		alert("Is that a number, pardner? \n (enter a valid number)");
+		return;
+	}
+	if(isNaN(stars)){
+		alert("We need to warn folks, pardner. \n (enter a number of stars between 1 and 5)");
+		return;
+	}
+	if(bountySum < 100){
+		alert("This whelp is no threat to anyone, kid. \n (enter a bounty of at least 100)");
 		return;
 	}
 	
 	const newPoster = document.createElement("div");
 	newPoster.className = "wanted-poster";
+	newPoster.style.position = "absolute";
 	newPoster.style.animation = "ripple 0.5s";
+	newPoster.style.visibility = "visible";
 	
 	const tack = document.createElement("div");
 	tack.className = "wall-tack";
 	newPoster.appendChild(tack);
+	tack.style.visibility = "visible";
 	
 	const face = document.createElement("img");
 	face.src = "images/" + outlaw;
@@ -78,18 +89,46 @@ function createNewPoster(outlaw, infamy, bountySum, stars){
 	
 	document.querySelector(".corkboard").appendChild(newPoster);
 	makeDraggable(newPoster, tack);
+	resolveCollisions(tack);
+}
+
+function counterScroll(target, value){
+	let startVal = parseInt(target.textContent.replace("Money: $", "")) || 0;
+	let curVal = startVal;
+	const increment = (value - startVal) / 300;
+	const interval = 20;
+	
+	const counter = setInterval(() => {
+		curVal += increment;
+		target.textContent = "Money: $" + Math.floor(curVal);
+		
+		if(curVal >= value){
+			clearInterval(counter);
+			target.textContent = "Money: $" + value;
+			const cashSound = document.getElementById("kaching");
+			const startTime = 4.5;
+			const endTime = 7.0;
+			cashSound.currentTime = startTime;
+			cashSound.play();
+			setTimeout(() =>{
+				cashSound.pause();
+			}, (endTime - startTime) * 1000);
+		}
+	}, interval);
 }
 
 function clearBounty(poster, bountySum){
-	const gunSound = document.getElementById("gunSound");
+	const gunSound = document.getElementById("blam");
 	gunSound.play();
-	const gunshot = document.createElement("div");
+	
+	const gunshot = document.createElement("img");
+	gunshot.src = "images/pow.png"; // https://creazilla.com/media/clipart/60300/pow
 	gunshot.className = "gunshot";
 	poster.appendChild(gunshot);
 	
 	setTimeout(() =>{
 		money += parseInt(bountySum, 10);
-		document.getElementById("moolah").textContent = "Money: $" + money;
+		counterScroll(document.getElementById("moolah"), money);
 		poster.style.animation = "slideDown 1s forwards";
 		
 		setTimeout(() =>{
@@ -108,16 +147,20 @@ document.getElementById("posting").onclick = function(){
 	const bountySum = document.getElementById("bountySum").value;
 	const stars = parseInt(document.getElementById("stars").value, 10);
 	createNewPoster(outlaw, infamy, bountySum, stars);
+	console.log("Closing the creation area");
 	document.getElementById("overlay").style.display = "none";
 };
-
+let z = 3;
 function makeDraggable(poster, tack){
 	let drag = false;
 	tack.onmousedown = function(event) {
 	    drag = true;
 	    let shiftX = event.clientX - tack.getBoundingClientRect().left;
 	    let shiftY = event.clientY - tack.getBoundingClientRect().top;
-
+		poster.style.zIndex = z;
+		tack.style.zIndex = z + 1;
+		z += 2;
+		
 	    function moveAt(pageX, pageY) {
 	        const cursorY = -90;
 	        poster.style.left = pageX - shiftX + 'px';
@@ -136,6 +179,7 @@ function makeDraggable(poster, tack){
 	        drag = false;
 	        document.removeEventListener('mousemove', onMouseMove);
 	        document.removeEventListener('mouseup', onMouseUp);
+			resolveCollisions(tack);
 	    }
 	};
 
@@ -143,3 +187,36 @@ function makeDraggable(poster, tack){
 	    return false;
 	};
 }
+
+function resolveCollisions(tack){
+	const tacks = document.querySelectorAll(".wall-tack");
+	let checks = 10;
+	while(checks > 0){
+		let collide = false;
+		tacks.forEach(tack2 => {
+			if(tack2 !== tack && overlap(tack, tack2)){
+				console.log(`Overlapping detected. Moving tack.`)
+				const dx = (Math.random() - 0.5) * 50;
+				const dy = (Math.random() - 0.5) * 50;
+				console.log(`Moving by dx: ${dx}, dy: ${dy}`);
+				tack.parentElement.style.left = `${tack.parentElement.offsetLeft + dx}px`;
+				tack.parentElement.style.top = `${tack.parentElement.offsetTop + dy}px`;
+				collide = true;
+			}
+		});
+		if(!collide){
+			break;
+		}
+	checks -= 1;
+	}
+}
+
+function overlap(tack1, tack2) {
+    const t1 = tack1.getBoundingClientRect();
+    const t2 = tack2.getBoundingClientRect();
+    const isOverlapping = !(t1.right < t2.left || t1.left > t2.right || t1.bottom < t2.top || t1.top > t2.bottom);
+	console.log(`Checking overlap: ${isOverlapping}`);
+	return isOverlapping;
+}
+
+
